d0e664cbc98ddc759dd21aaf20e8fecb5ccd5008
Vyacheslav Lapin <vyacheslav@y-tree.com>
2020-01-27 17:14:06 +0300

    tests & lombok config added

--- src/test/java/ru/vlapin/courses/spring/springbootmvc/SpringBootMvcApplicationTests.java
+++ src/test/java/ru/vlapin/courses/spring/springbootmvc/SpringBootMvcApplicationTests.java
@@ -1,15 +1,37 @@
 package ru.vlapin.courses.spring.springbootmvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+//@org.springframework.security.test.context.support.WithMockUser(authorities = "ADMIN")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class SpringBootMvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  MockMvc mockMvc;
 
+  @Test
+  void contextLoads() {
+  }
 
+  @SneakyThrows
+  @Test
+  @DisplayName("page input params")
+  void pageInputParamsTest() {
+		mockMvc.perform(get("/pages/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("text/html;charset=UTF-8"));
+  }
 
 }
